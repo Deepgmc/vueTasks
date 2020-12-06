08ae190791b08d6b73571e6e9b42e9af55439539
Deepgmc <deepgmc_job@mail.ru>
2020-12-06 14:50:07 +0300

    Vue charts to history page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,24 +1,33 @@
 <template>
    <div>
-   <div class="page-title">
-      <h3>Профиль</h3>
-   </div>
-
-   <form class="form">
-      <div class="input-field">
-         <input
-            id="description"
-            type="text"
-         >
-         <label for="description">Имя</label>
-         <span
-               class="helper-text invalid">name</span>
+      <div class="page-title">
+         <h3>Профиль</h3>
       </div>
 
-      <button class="btn waves-effect waves-light" type="submit">
-         Обновить
-         <i class="material-icons right">send</i>
-      </button>
-   </form>
+      <form class="form">
+         <div class="input-field">
+            <input
+               id="description"
+               type="text"
+            >
+            <label for="description">Имя</label>
+            <span
+                  class="helper-text invalid">name</span>
+         </div>
+
+         <div class="switch">
+            <label>
+               Off
+               <input type="checkbox">
+               <span class="lever"></span>
+               On
+            </label>
+         </div>
+
+         <button class="btn waves-effect waves-light" type="submit">
+            Обновить
+            <i class="material-icons right">send</i>
+         </button>
+      </form>
    </div>
 </template>
